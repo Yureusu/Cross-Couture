d75fc546ef0a6d0bdbbd83f8d365dfe220e55a52
Yureusu <yureusu9@gmail.com>
2025-02-02 19:06:01 +0800

    480 update

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function openFfollow(){
+    document.getElementById('afollow').style.display = 'flex';
+}
+function openFbrand(){
+    document.getElementById('abrand').style.display = 'flex';
+}
+function openFcare(){
+    document.getElementById('acare').style.display = 'flex';
+}
+function openFhelp(){
+    document.getElementById('ahelp').style.display = 'flex';
+}
